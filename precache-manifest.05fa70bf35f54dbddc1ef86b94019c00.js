30373878794f9356194aef709c3d301783054f42
Nikolas-Bars <kasp2409@mail.ru>
2022-03-19 15:12:49 +0300

    Updates

---
+++ precache-manifest.05fa70bf35f54dbddc1ef86b94019c00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28c568eb93b74fd4dead924621b0124",
+    "url": "/MyHomework/index.html"
+  },
+  {
+    "revision": "10bf9aecc8031ff55c5d",
+    "url": "/MyHomework/static/css/main.db6c21ed.chunk.css"
+  },
+  {
+    "revision": "a6477b9e5bcb048edf21",
+    "url": "/MyHomework/static/js/2.e2453097.chunk.js"
+  },
+  {
+    "revision": "abc28c365c609ffc9a2e2e165c7e73d8",
+    "url": "/MyHomework/static/js/2.e2453097.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10bf9aecc8031ff55c5d",
+    "url": "/MyHomework/static/js/main.830a71c7.chunk.js"
+  },
+  {
+    "revision": "58c6618b9e451848ed5b",
+    "url": "/MyHomework/static/js/runtime-main.ba7cd339.js"
+  }
+]);
